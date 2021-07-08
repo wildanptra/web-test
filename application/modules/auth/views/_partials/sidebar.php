c54a2373b6714fbe39ee518ecffd4aa075ee9047
wildanptra <wildanptra03@gmail.com>
2021-07-08 15:44:10 +0700

    Update sidebar.php

--- application/modules/auth/views/_partials/sidebar.php
+++ application/modules/auth/views/_partials/sidebar.php
@@ -8,18 +8,6 @@
                 Dashboard
             </a>
 
-            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseCategory" aria-expanded="false" aria-controls="collapseCategory">
-                <div class="sb-nav-link-icon"><i class="fas fa-list-alt"></i></div>
-                Category
-                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-            </a>
-            <div class="collapse" id="collapseCategory" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                <nav class="sb-sidenav-menu-nested nav">
-                    <a class="nav-link" href="<?= site_url('auth/category') ?>"> <div class="sb-nav-link-icon"><i class="fas fa-folder"></i>
-                    </div> Data Category </a>
-                </nav>
-            </div>
-
             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseProduct" aria-expanded="false" aria-controls="collapseProduct">
                 <div class="sb-nav-link-icon"><i class="fas fa-list-alt"></i></div>
                 Product
@@ -29,6 +17,9 @@
                 <nav class="sb-sidenav-menu-nested nav">
                     <a class="nav-link" href="<?= site_url('auth/product') ?>"> <div class="sb-nav-link-icon"><i class="fas fa-folder"></i>
                     </div> Data Product </a>
+
+                    <a class="nav-link" href="<?= site_url('auth/category') ?>"> <div class="sb-nav-link-icon"><i class="fas fa-folder"></i>
+                    </div> Data Category </a>
                 </nav>
             </div>
 
